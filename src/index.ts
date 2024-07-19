030fe7b82390e1457bc10ca05cf25cbb6e8ac78b
alvin <alvin@mabolotech.com>
2024-07-19 23:04:26 +0800

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import connectToDatabase from "./config/db";
 import poster from "./router/poster";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,8 +9,8 @@ const PORT = process.env.PORT || 3001;
 // Middleware
 app.use(express.json());
 connectToDatabase();
-console.log(process.env.CLOUD_NAME);
 
+app.use(cors);
 app.use(poster);
 // Start the server
 app.listen(PORT, () => {
